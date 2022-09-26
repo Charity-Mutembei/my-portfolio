8c05b84c7989b953fd1717e28bed77c25d5dab26
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-09-27 02:23:49 +0300

    Feat: Project section added & routes installed

--- src/components/ThirdPage/index.js
+++ src/components/ThirdPage/index.js
@@ -2,6 +2,12 @@ import './index.scss'
 import AuthPage from './../../assets/PROJECTS/PROJECT1/AUTH.PNG'
 import AuthPageAfter from './../../assets/PROJECTS/PROJECT1/After-Auth1.PNG'
 import AfterPageAfter2 from './../../assets/PROJECTS/PROJECT1/After-Auth2.PNG'
+import FirstPage from './../../assets/PROJECTS/PROJECT2/FirstPage.PNG'
+import SecondtPage from './../../assets/PROJECTS/PROJECT2/SecondPage.PNG'
+import ThirdPage from './../../assets/PROJECTS/PROJECT2/ThirdPage.PNG'
+import SupportPage from './../../assets/PROJECTS/PROJECT3/SuportPage.PNG'
+import LandingPage from './../../assets/PROJECTS/PROJECT3/LandingPage.PNG'
+import AbortionPage from './../../assets/PROJECTS/PROJECT3/AbortionPage.PNG'
 
 const Project = () => {
     return (
@@ -51,6 +57,89 @@ const Project = () => {
                     </div>
                 </div>
             </div>
+            {/* second project showcase  */}           
+            <div class="row" >
+                <div class="col-5" id="parent">
+                <img src={ThirdPage} alt="...." id="image"/>               
+                <img src={SecondtPage} alt="...." id="image1"/>                   
+                <img src={FirstPage} alt="...." id="image2"/>
+
+                </div>
+                <div class="col-7" id="rightSide" >
+                
+                    <div className="projectTitle">
+                        <a href="https://charity-mutembei.github.io/ANGULAR2-2/">
+                        <h1 className="projectName"> <span class="number">2</span> GitFind <i class="fas fa-location-arrow"></i></h1>
+                        </a>
+                    </div>
+                    <div className="projectDescription">
+                        <p>
+                        This is an application meant to be a search engine for git profiles 
+                        and repositories found. A user is able to search for a profile or 
+                        the repositories of the GitHub account filled in the search bar.
+                        </p>
+                    </div>
+                    <div className="languages">
+                         <div className="badges">
+                    
+                            <div class="badge blue" id="yellow">
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
+                                    <path fill="#000000" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169    c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z"/>
+                            </svg>
+                            <p class="title">LANGUAGES <br/> USED</p>
+                            <p class="subtitle">
+                                Angular <br/> JavaScript <br/> HTML <br/> CSS
+                            </p>
+                            </div>
+                            
+                            </div>                        
+                    </div>
+                </div>
+            </div>
+            {/* end of second project  */}
+
+            {/* third project  */}          
+            <div class="row" >
+                <div class="col-5" id="parent">
+                <img src={SupportPage} alt="...." id="image"/>               
+                <img src={AbortionPage} alt="...." id="image1"/>                   
+                <img src={LandingPage} alt="...." id="image2"/>
+
+                </div>
+                <div class="col-7" id="rightSide" >
+                
+                    <div className="projectTitle">
+                        <a href="https://charity-bit.github.io/sexucate/">
+                        <h1 className="projectName"> <span class="number">3</span> Sexucate <i class="fas fa-location-arrow"></i></h1>
+                        </a>
+                    </div>
+                    <div className="projectDescription">
+                        <p>
+                        Sexucate is a platform that aims to reduce stigmatization
+                         of topics concerning sexual and reproductive health and practices 
+                         in young adolescents by providing information to these young 
+                         girls and boys on safe sex practices.
+
+                        </p>
+                    </div>
+                    <div className="languages">
+                         <div className="badges">
+                    
+                            <div class="badge green" id="yellow">
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
+                                    <path fill="#000000" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169    c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z"/>
+                            </svg>
+                            <p class="title">LANGUAGES <br/> USED</p>
+                            <p class="subtitle">
+                                Python <br/> JavaScript <br/> HTML <br/> CSS
+                            </p>
+                            </div>
+                            
+                            </div>                        
+                    </div>
+                </div>
+            </div>
+            {/* end of third project  */}
             </div>
         </div>
         </>
