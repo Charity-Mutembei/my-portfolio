7bd9159a9bdc75c2dcefa203d147254e089c4d36
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-09-27 03:17:16 +0300

    Feat: Project section added & routes installed

--- src/components/ThirdPage/index.js
+++ src/components/ThirdPage/index.js
@@ -8,6 +8,9 @@ import ThirdPage from './../../assets/PROJECTS/PROJECT2/ThirdPage.PNG'
 import SupportPage from './../../assets/PROJECTS/PROJECT3/SuportPage.PNG'
 import LandingPage from './../../assets/PROJECTS/PROJECT3/LandingPage.PNG'
 import AbortionPage from './../../assets/PROJECTS/PROJECT3/AbortionPage.PNG'
+import FrontPage from './../../assets/PROJECTS/PROJECT4/FrontPage.PNG'
+import MiddlePage from './../../assets/PROJECTS/PROJECT4/MiddlePage.PNG'
+import BackPage from './../../assets/PROJECTS/PROJECT4/BackPage.PNG'
 
 const Project = () => {
     return (
@@ -140,6 +143,50 @@ const Project = () => {
                 </div>
             </div>
             {/* end of third project  */}
+            {/* start of pag 4 */}
+            <div class="row" >
+                <div class="col-5" id="parent">
+                <img src={BackPage} alt="...." id="image"/>               
+                <img src={MiddlePage} alt="...." id="image1"/>                   
+                <img src={FrontPage} alt="...." id="image2"/>
+
+                </div>
+                <div class="col-7" id="rightSide" >
+                
+                    <div className="projectTitle">
+                       
+                        <h1 className="projectName"> <span class="number">4</span>  ELIMISHA KENYA <i class="fas fa-location-arrow"></i></h1>
+              
+                    </div>
+                    <div className="projectDescription">
+                        <p>
+                        A personal project aiming to aid students of the lower grades in being 
+                        outcome-focused, solution-based, efficient in planning and organizing
+                         their study time as well as goals and objectives. <br/> This initiative is
+                         influenced by the challenges faced by all slow learners who have a
+                          hard time keeping up with the pace of the school curriculum yet their
+                           assessment is inconsiderate of their nature and state when learning
+
+                        </p>
+                    </div>
+                    <div className="languages">
+                         <div className="badges">
+                    
+                            <div class="badge green" id="yellow">
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
+                                    <path fill="#000000" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169    c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z"/>
+                            </svg>
+                            <p class="title">LANGUAGES <br/> USED</p>
+                            <p class="subtitle">
+                                Python <br/> JavaScript <br/> HTML <br/> CSS / SCSS
+                            </p>
+                            </div>
+                            
+                            </div>                        
+                    </div>
+                </div>
+            </div>
+            {/* end of page 4 */}
             </div>
         </div>
         </>
